9b8b2dd700266d27d8a9474c485985bcb350541a
NHMHieuA1K57 <124542355+NHMHieuA1K57@users.noreply.github.com>
2024-10-02 22:41:06 +0700

    v1

---
+++ admin/src/App.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Admin from './Pages/Admin/Admin'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Admin/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className='descriptionbox-navigator'>
+          <div className='descriptionbox-nav-box'>Description</div>
+          <div className='descriptionbox-nav-box fade'>Reviews(122)</div>
+
+      </div><div className='descriptionbox-description'>
+              <p>Discover the ultimate blend of style and comfort with our premium leather handbag. Designed for modern women on the go, it features a spacious interior, adjustable straps, and elegant gold-tone hardware. Perfect for daily use, it keeps your essentials organized in style. Available in classic black, tan, and red.
+              </p>
+              <p>Introducing our premium wireless earbuds, engineered for superior sound quality and designed with your comfort in mind. These sleek earbuds feature cutting-edge noise cancellation technology, allowing you to immerse yourself fully in your favorite music or podcasts, free from distractions. With up to 20 hours of battery life and a compact, portable charging case, these earbuds are perfect for on-the-go use. The intuitive touch controls and seamless Bluetooth connectivity make them easy to use, while the ergonomic design ensures a secure and comfortable fit for all-day wear. Available in a range of stylish colors, these wireless earbuds are the ultimate accessory for music lovers and tech enthusiasts alike.
+              </p>
+          </div></div>
+    
+  )
+}
+
+export default DescriptionBox
